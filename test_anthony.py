4c946dedafe5280543bbc07c03bc7b4d64ab0dd8
VentoAureo230 <anthony.meny35@gmail.com>
2022-10-18 16:40:17 +0200

    changement du test victoire de fin de game

--- test_anthony.py
+++ test_anthony.py
@@ -5,9 +5,22 @@ import models
 
 class TestFunctionCardGame(unittest.TestCase):
 
-    def test_end_game(self):
+    def test_victory(self):
+        """Test la fin du jeu quand le joueur (humain pas IA) n'a plus de carte en main"""
         game = models.PresidentGame()
         player_1 = game.players[0]
-        print(player_1.hand)
         self.assertTrue(len(player_1.hand) < 1)
         self.assertFalse(len(player_1.hand) > 1)
+
+    def test_end_game(self):
+        """Affche la fin de game quand il reste 1 seul joueur avec des cartes en main
+         TODO :  doit attribuer le rôle de troufion et président en fin de game si le joueur relance une partie
+        """
+        game = models.PresidentGame()
+        players = len(game.players)
+        self.assertTrue(len(players.hand) < 2)
+
+    def test_round(self):
+        """Compte le nombre de rounds dans une partie ?"""
+        game = models.PresidentGame()
+        pass
