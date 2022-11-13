bf65c8ee7b8c81137b7c2a45432b0f6b6c872efc
VentoAureo230 <anthony.meny35@gmail.com>
2022-11-14 00:15:33 +0100

    Ajout du main menu

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 import random
-
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import showinfo, askquestion
+from PIL import Image, ImageTk
 import names
-
 
 # from random import *
 COLORS = ['♡', '♤', '♧', '♢']
@@ -20,8 +23,6 @@ VALUES = {
     'R': 13,
     'A': 14
 }
-
-
 
 
 class Deck:
@@ -96,11 +97,11 @@ class Player:
     is_president = False
     is_trouduc = False
 
-    def __init__(self, player_name=None,id=None):
-        if self.is_human==True:
-            player_name='You'
+    def __init__(self, player_name=None, id=None):
+        if self.is_human == True:
+            player_name = 'You'
         self._name: str = player_name if player_name is not None else names.get_first_name()
-        self._id=id
+        self._id = id
         self._hand: list = []
 
     def __eq__(self, other):
@@ -142,7 +143,7 @@ class Player:
         return cards_played
 
     def __repr__(self):
-        return str(self._id)#f"{self.name}\t: {self.hand}"
+        return str(self._id)  # f"{self.name}\t: {self.hand}"
 
     def has_symbol(self, card_symbol) -> int:
         nb_cards = 0
@@ -151,13 +152,13 @@ class Player:
                 nb_cards += 1
         return nb_cards
 
+
 class AIPlayer(Player):
     is_human = False
 
     def play(self, choices, nb_cards: int) -> list:
         """
         Play a card corresponding to what has been played on the table.
-        TODO: Implement an AI
         Args:
             choice: The minimum card value to play.
             nb_cards: The number of cards to play.
@@ -166,9 +167,9 @@ class AIPlayer(Player):
 
         """
         if choices is not None:
-            choice=choices[0]
+            choice = choices[0]
         else:
-            choice=None
+            choice = None
         if choice is None:
             choice = self.hand[random.randint(0, len(self.hand) - 1)]
         best_choice = None
@@ -187,13 +188,13 @@ class PresidentGame:
         self.__generate_cards()
         self.round = 0
         self.nb_players = nb_players
-        self.troufion=None
-        self.president=None
+        self.troufion = None
+        self.president = None
 
     def __generate_players(self, nb_players: int):
-        self.__players = [Player(None,0)]
+        self.__players = [Player(None, 0)]
         for i in range(nb_players - 1):
-            self.__players.append(AIPlayer(None,i+1))
+            self.__players.append(AIPlayer(None, i + 1))
 
     def __generate_cards(self):
         self.__deck = Deck()
@@ -209,27 +210,28 @@ class PresidentGame:
             giving_card_to_player = (giving_card_to_player + 1) % nb_players
         if self.president is not None and self.troufion is not None:
             self.cards_switch()
+
     def cards_switch(self):
-        for _ in range (2):
-            troufion_card=self.troufion.hand.pop(-1)
-            i=0
-            hand=self.president.hand
-            while hand[i].value<troufion_card.value and i<len(hand):
-                i+=1
-            self.president.hand.insert(i,troufion_card)
-            i=0
-            president_card=self.president.hand.pop(0)
-            hand=self.troufion.hand
-            while hand[i].value<president_card.value and i<len(hand):
-                i+=1
-            self.troufion.hand.insert(i,president_card)
-        print(self.president.hand,self.troufion.hand)
+        for _ in range(2):
+            troufion_card = self.troufion.hand.pop(-1)
+            i = 0
+            hand = self.president.hand
+            while hand[i].value < troufion_card.value and i < len(hand):
+                i += 1
+            self.president.hand.insert(i, troufion_card)
+            i = 0
+            president_card = self.president.hand.pop(0)
+            hand = self.troufion.hand
+            while hand[i].value < president_card.value and i < len(hand):
+                i += 1
+            self.troufion.hand.insert(i, president_card)
+        print(self.president.hand, self.troufion.hand)
 
     def player_active(self):
         """Un joueur est défini comme actif s'il a au moins une carte en main"""
         return [player for player in self.__players if len(player.hand) > 0]
 
-    def human_asked_to_play(self,player):
+    def human_asked_to_play(self, player):
         if self.previous_cards_played is not None:
             self.nb_cards_round = len(self.previous_cards_played)
             self.is_first_turn = False
@@ -240,7 +242,8 @@ class PresidentGame:
         print(player.hand, )
         player_choice = None
         choice_player_value = 0
-        while (player.has_symbol(player_choice) < self.nb_cards_round or self.previous_cards_played_value() > choice_player_value) and player_choice != 'ff':
+        while (player.has_symbol(
+                player_choice) < self.nb_cards_round or self.previous_cards_played_value() > choice_player_value) and player_choice != 'ff':
             player_choice = input('What value do you wish to play ? ')
             if player_choice != 'ff':
                 try:
@@ -256,25 +259,27 @@ class PresidentGame:
         if player_choice != 'ff':
             choice = player.play(player_choice, self.nb_cards_round)
             print(f"You play {self.previous_cards_played}")
-            self.previous_cards_played=choice
+            self.previous_cards_played = choice
         else:
             print(f"You skipped")
-            choice=None
+            choice = None
         return choice
-    def player_ran_out_of_cards(self,player):
+
+    def player_ran_out_of_cards(self, player):
         print(player.name, "n'as plus de carte")
         self.players_list_still_has_card.remove(player)
         self.players_to_remove.append(player)
         if self.president == None:
             self.president = player
             player.is_president = True
+
     def round_over(self):
-        if len(self.players_list_still_has_card) == 1:#game over
+        if len(self.players_list_still_has_card) == 1:  # game over
             self.troufion = self.players_list_still_has_card[0]
             self.troufion.is_trouduc = True
             self.players_list_still_has_card[0]._hand = []
             self.active_game = False
-        else:#new round
+        else:  # new round
             round_winner = self.still_playing_round[0].name
             print(round_winner, 'gagne, fin de la manche')
             j = 0
@@ -289,6 +294,7 @@ class PresidentGame:
                         if j >= len(self.players_list_still_has_card):
                             j = 0
                 j += 1
+
     def game_loop(self):
         """
         The main game loop.
@@ -296,7 +302,7 @@ class PresidentGame:
         Args:
             g: The President Game instance.
         """
-        wanna_continue=True
+        wanna_continue = True
         while wanna_continue:
             self.distribute_cards()
             self.still_playing_round = [x for x in self.players]
@@ -308,26 +314,26 @@ class PresidentGame:
                     print(joueur.name, "has", len(joueur.hand), "cards")
                 self.previous_cards_played = None
                 self.players_to_remove = []
-                i=0
+                i = 0
                 while len(self.still_playing_round) > 1:
                     if i > len(self.still_playing_round) - 1:
                         i = 0
-                    player=self.still_playing_round[i]
+                    player = self.still_playing_round[i]
 
-                    if player.is_human == True:#human
-                        player_choice=self.human_asked_to_play(player)
+                    if player.is_human == True:  # human
+                        player_choice = self.human_asked_to_play(player)
 
-                    else:#bot
-                        player_choice=player.play(self.previous_cards_played, self.nb_cards_round)
+                    else:  # bot
+                        player_choice = player.play(self.previous_cards_played, self.nb_cards_round)
                         if player_choice is not None:
-                            self.previous_cards_played=player_choice
+                            self.previous_cards_played = player_choice
 
                     if len(player.hand) < 1:
                         self.player_ran_out_of_cards(player)
 
                     # Update the latest card played
                     elif player_choice is not None:
-                        i+=1
+                        i += 1
                     else:
                         self.players_to_remove.append(player)
 
@@ -336,18 +342,19 @@ class PresidentGame:
                     self.players_to_remove = []
                 self.round_over()
 
-            print('Thank you for playing. I hope you enjoyed !')
             response_user = input('continue O/N')
             if response_user == 'o' or response_user == 'O':
                 wanna_continue = True
             else:
                 wanna_continue = False
+
     def previous_cards_played_value(self):
         if self.previous_cards_played is not None:
-            previous_cards_played_value=self.previous_cards_played[0].value
+            previous_cards_played_value = self.previous_cards_played[0].value
         else:
-            previous_cards_played_value =0
-        return  previous_cards_played_value
+            previous_cards_played_value = 0
+        return previous_cards_played_value
+
     @property
     def players(self):
         return self.__players
@@ -360,3 +367,149 @@ class PresidentGame:
     def main_player(self):
         """ Main player is player 0 """
         return self.__players[0]
+
+
+class Root(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title('Jeu du président')
+        self.configure(bg='green')
+        self.geometry('1200x800')
+        self.resizable(height=False, width=False)
+
+        """Homepage du jeu"""
+        self.home = Frame(self, bg="green")
+        self.btn_play = Button(self.home, text="Jouer", command=lambda: [self.hide_homepage(), self.new_game()])
+        self.btn_parameters = Button(self.home, text="Paramètre",
+                                     command=lambda: [self.display_parameters(), self.hide_homepage()])
+        self.btn_parameters.pack()
+        self.btn_play.pack()
+        self.display_homepage()
+
+        # Ecran de jeu
+        self.play = Frame(self, bg="green")
+        self.input_player = Entry(self.play)
+        self.input_player.pack()
+        self.player_label = Label(self.play, text="Combien de joueur souhaitez-vous dans vos parties ?")
+        self.player_label.pack()
+        self.setup_ui()
+        self.mainloop()
+
+    def setup_ui(self):
+        """Gestion de la barre de navigation de l'application"""
+        menubar = tkinter.Menu(self)
+        # Barre de menu
+        menu1 = Menu(tearoff=0)
+        menu1.add_command(label="Nouvelle partie", command=self.restart_game)
+        menu1.add_command(label="Abandonner", command=self.forfeit)
+        menu1.add_separator()
+        menu1.add_command(label="Quitter", command=self.quit)
+        menubar.add_cascade(label="Jeu", menu=menu1)
+
+        menu2 = Menu(tearoff=0)
+        menu2.add_command(label="A propos", command=self.info)
+        menubar.add_cascade(label="Crédit", menu=menu2)
+
+        self.config(menu=menubar)
+
+    def new_game(self):
+        """Démarrage de la partie"""
+        g = PresidentGame()
+
+        self.game = Frame(self)
+        self.btn_return = Button(self.game, text="Homepage",
+                                 command=lambda: [self.display_homepage(), self.hide_game()])
+        self.btn_return.pack()
+
+        self.game.pack()
+
+    def restart_game(self):
+        """Lance une nouvelle partie depuis l'onglet jeu dans la barre de navigation"""
+        result = askquestion("Nouvelle partie",
+                             "Voulez-vous relancer une partie ? Si une partie est en cours elle ne sera pas "
+                             "sauvegardée.",
+                             icon='info')
+        if result == 'yes':
+            self.new_game()
+        else:
+            pass
+
+    def forfeit(self):
+        """Quitte la partie en cours"""
+        result = askquestion("Abandon", "Voulez-vous abandonner ?", icon="warning")
+        if result == 'yes':
+            self.quit()
+        else:
+            pass
+
+    def info(self):
+        """Affiche les crédits"""
+        result = showinfo("Crédit", "Jeu du président développer par Anthony Mény et Gillian Charra", icon="info")
+        if result == 'yes':
+            pass
+        else:
+            pass
+
+    def set_size(self, size):
+        """Gère la taille de l'écran"""
+        self.geometry(size)
+
+    def display_homepage(self):
+        self.home.pack()
+
+    def hide_homepage(self):
+        self.home.forget()
+
+    def display_parameters(self):
+        self.parameters = Frame(self)
+        self.size_label = Label(self.parameters, text="Sélectionnez une taille d'écran :")
+        self.size_label.pack()
+        self.size_label_800_450 = Button(self.parameters, text="800x450", command=lambda: self.set_size('800x450'))
+        self.size_label_800_450.pack()
+        self.size_label_1200_800 = Button(self.parameters, text="1200x800", command=lambda: self.set_size('1200x800'))
+        self.size_label_1200_800.pack()
+        self.player_name_label = Label(self.parameters, text="Votre pseudo :")
+        self.player_name_label.pack()
+        self.player_name_input = Entry(self.parameters, validate='focus')
+        self.player_name_input.pack()
+
+        self.btn_return = Button(self.parameters, text="Retour",
+                                 command=lambda: [self.display_homepage(), self.hide_parameters()])
+        self.btn_return.pack()
+        self.parameters.pack()
+
+    def hide_parameters(self):
+        self.parameters.forget()
+
+    def display_game_screen(self):
+        pass
+
+    def resize_cards(card):
+        """Affiche les cartes et garde le ratio de la carte en fonction de l'écran de base"""
+        card_img = Image.open(card)
+        card_resized = card_img.resize((150, 218))
+
+        global card_image
+        card_image = ImageTk.PhotoImage(card_resized)
+
+        return card_image
+
+    def player_hand(self):
+        """Affiche les cartes du joueur dans une section en bas de l'écran"""
+        player_frame = Frame(self, bg="green")
+        player_frame.pack()
+
+        card_in_frame = LabelFrame(player_frame, text="Ma main", bd=0)
+        card_in_frame.grid(row=0, column=0, padx=20, ipadx=20)
+
+    def enemy_hand(self):
+        for enemy in len(PresidentGame.nb_players) - 1:
+            enemy_frame = Frame(self, bg="green")
+            enemy_frame.pack()
+
+            card_in_frame = LabelFrame(enemy_frame, text='{}', bd=0)
+            card_in_frame.pack(padx=20, ipadx=20)
+
+    def hide_game(self):
+        self.game.forget()
